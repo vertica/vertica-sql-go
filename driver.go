d35e5a8b3b998635d4a9f8668abac4dcf18576fb
huebnerr <roger.huebner@microfocus.com>
2020-02-25 09:38:28 -0600

    update version to 0.2.0

--- driver.go
+++ driver.go
@@ -46,7 +46,7 @@ type Driver struct{}
 
 const (
 	driverName      string = "vertica-sql-go"
-	driverVersion   string = "0.1.7"
+	driverVersion   string = "0.2.0"
 	protocolVersion uint32 = 0x00030008
 )
 
