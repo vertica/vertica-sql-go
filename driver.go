50d0521f72e97266934833daab860c038db81134
Siting Ren <sitingren@hotmail.com>
2021-12-10 16:39:01 +0800

    Updated version to 1.2.1 (#126)

--- driver.go
+++ driver.go
@@ -46,7 +46,7 @@ type Driver struct{}
 
 const (
 	driverName      string = "vertica-sql-go"
-	driverVersion   string = "1.2.0"
+	driverVersion   string = "1.2.1"
 	protocolVersion uint32 = 0x00030009
 )
 
