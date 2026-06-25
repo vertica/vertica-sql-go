b702f27407e158a8c9d187d1c07dd0010076158e
sivaalampally <salampally@opentext.com>
2026-06-25 10:16:45 -0400

    Additional tests for different file types

---
+++ resources/tests/driver_test/test_copy_local_json_pre.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS json_values;
+CREATE TABLE json_values (name varchar(64), id int);
---
+++ resources/tests/driver_test/test_copy_local_pipe_pre.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS pipe_values;
+CREATE TABLE pipe_values (name varchar(64), id int);
